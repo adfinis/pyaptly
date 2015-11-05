875659e16e388c809d1fd2babfc2d6dcb1417bb6
David Vogt <david.vogt@adfinis-sygroup.ch>
2015-11-05 10:58:36 +0100

    Improve error handling and handle arguments correctly in all cases

--- pyaptly/__init__.py
+++ pyaptly/__init__.py
@@ -438,10 +438,10 @@ def expand_timestamped_name(name, timestamp_config, date=None):
     if '%T' not in name:
         return name
 
-    timestamp_info = timestamp_config.get('timestamp', {})
+    timestamp_info = timestamp_config.get('timestamp', timestamp_config)
     config_time    = timestamp_info.get('time', 'FAIL')
     if config_time == 'FAIL':
-        raise ValueError("Timestamp config has no valid time entry")
+        raise ValueError("Timestamp config has no valid time entry: %s" % str(timestamp_config))
 
     config_repeat_weekly = timestamp_info.get('repeat-weekly', None)
 
